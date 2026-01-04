a6dabc5988b53e8182d1efa6ee22d687c5a575c5
Mochammad khaerul ilman <110533356+khaerulilman@users.noreply.github.com>
2026-01-05 00:39:40 +0700

    Refactor App component to include Footer and layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './page/Home'
 import About from './page/about'
 import Navbar from './layout/Navbar'
+import Footer from './layout/Footer'
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* 🔥 Route kategori */}
-        <Route path="/category/:category" element={<Home />} />
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
 
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </>
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/category/:category" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
